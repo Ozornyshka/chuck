a2e69ebb5d69c48eb05ab56c38efad6738e275af
Frontend Vlad <frontend.vlad@sbstudio.ent>
2021-09-21 11:22:04 +0300

    toolkit done

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -5,15 +5,4 @@ let store = configureStore({
   reducer: rootReducer,
 });
 
-// function saveToLocalStorage(state) {
-//   try {
-//     const favouriteState = JSON.stringify(state);
-//     localStorage.setItem("persistantState", favouriteState);
-//   } catch (e) {
-//     console.warn(e);
-//   }
-// }
-
-// store.subscribe(() => saveToLocalStorage(store.getState().favourite));
-
 export default store;
--- src/components/FormSearch/store/jokeSlice.js
+++ src/components/FormSearch/store/jokeSlice.js
@@ -1,4 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchRandomJoke = createAsyncThunk(
+  "joke/fetchRandomJoke",
+  async function (_, { dispatch }) {
+    const response = await axios("https://api.chucknorris.io/jokes/random");
+    const data = await response.data;
+    dispatch(getJoke(data));
+  }
+);
+export const fetchSearchCategories = createAsyncThunk(
+  "joke/fetchSearchCategories",
+  async function (searchToCategories, { dispatch }) {
+    const response = await axios(
+      `https://api.chucknorris.io/jokes/random?category=${searchToCategories}`
+    );
+    const data = await response.data;
+    dispatch(getJoke(data));
+  }
+);
+export const fetchSearchInput = createAsyncThunk(
+  "joke/fetchSearchInput",
+  async function (inputText, { dispatch }) {
+    const response = await axios(
+      `https://api.chucknorris.io/jokes/search?query=${inputText}`
+    );
+    const data = await response.data;
+    dispatch(
+      getJoke(data.result[Math.floor(Math.random() * data.result.length - 1)])
+    );
+  }
+);
 
 const jokeSlice = createSlice({
   name: "joke",
